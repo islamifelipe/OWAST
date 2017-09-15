43795e983dc0d204175bb15f9c6246a0fa5c0728
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-09-15 14:23:03 -0300

    memetic

---
+++ memetic/param.h
@@ -0,0 +1,60 @@
+#ifndef ZG_H
+#define ZG_H
+
+/*This code file was kindly provided by Monteiro */
+
+#include "rand/randomc.h"
+
+/* OPCOES DE DEBUG */
+//#define DEBUG
+#ifdef DEBUG
+#define D(x) x
+#endif
+#ifndef DEBUG
+#define D(x)
+#endif
+
+//#define ASSERT
+#ifdef ASSERT
+#define ASS(x) x
+#endif
+#ifndef ASSERT
+#define ASS(x)
+#endif
+
+#define f(k,i,j) custos[k][i][j] // objetivo k, vertice i j 
+#define EPS 1e-9 // quanquer coisa menor que esse valor, é considerado 0
+
+#define NUMOBJETIVOS 8 // Varia
+#define NUMEROVERTICES 15 // TESTE
+#define NUMEROARESTAS (NUMEROVERTICES-1)
+
+
+/*Parâmetros do SPEA/R */
+#define K_LAYER 5
+#define NUMDIRECOES ((NUMOBJETIVOS*K_LAYER*(K_LAYER+3))/2+1)
+
+/*Parâmetros da populçao*/
+#define TAMANHOPOPULACAO (NUMDIRECOES) //#define TAMANHOPOPULACAO 100
+#define PORCENT_RANDOM 25
+#define PORCENT_KCENTRUM 25
+#define PORCENT_KTRIMENT 25
+#define PORCENT_HURWICZ 25
+
+// Parametros do transgenético
+
+#define NUMESCVECTORS 10
+#define NUMTRANSPONSONS 2
+#define NUMPLASMIDEOS 10
+#define MAXIMOFALHASBUSCA 2
+
+#define NUMSOLTRANSPOSON 3
+
+#define NUMEROITERACOES 30 /* FIXME 500 */
+#define NUMEROMCPRIM 142   /* NUMERO DE SOLUCOES INICIAIS GERADAS PELO MCPRIM */
+#define ALGORITMOCONVERGIU 10 /* NUMERO DE GERACOES SEM MUDANCA NO PARETO SET PARA DIVERSIFICAR */
+#define PCTDIVERSIFICACAO 0.0 /* PARCELA DA POPULACAO A SER ALTERADA NA DIVERSIFICACAO */
+
+#endif
+
+
---
+++ memetic/transgenetico.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <iostream>
+
+#include "rmcPrim.cpp"
+#include "SolucaoEdgeSet.cpp"
+#include "popInicial.cpp"
+#include "vetoresDirecao.cpp"
+using namespace std;
+
+double custos[NUMOBJETIVOS][NUMEROVERTICES][NUMEROVERTICES];
+double w[NUMOBJETIVOS];  // pesos OWA
+SolucaoEdgeSet *populacao[TAMANHOPOPULACAO];
+double vetoresDirecoes[NUMDIRECOES][NUMOBJETIVOS];
+
+void input(){
+	int n,p; // esta leitura de n e p é somente para cumprir o formato da instância. Os valores de fato estao em param.h
+	cin>>n;
+	cin>>p;
+	for (int i=0; i<NUMOBJETIVOS; i++){
+		cin>>w[i];
+	}
+	int org, dest;
+	for (int i=0;i<NUMEROVERTICES-1;i++) {
+		for (int j=i+1;j<NUMEROVERTICES;j++) {
+			cin>>org;
+			cin>>dest;
+			if (org!=i) cout<<"ERRO Leitura 1"<<endl;
+			if (dest!=j) cout<<"ERRO Leitura 2"<<endl;
+			for (int ob = 0; ob<NUMOBJETIVOS; ob++){
+				cin>>custos[ob][i][j];
+			}
+		}
+	}
+}
+
+int main(){
+
+	input(); // ler instância
+	TRandomMersenne rg( 4549589 );
+	Reference_Generation(vetoresDirecoes);
+	alocaPopulacao(populacao, rg); // aloca populaçao inicial
+	gerarPopulacao2(populacao, rg,vetoresDirecoes); // gera populaçao inicial
+	for (int i=0; i<TAMANHOPOPULACAO; i++){
+		cout<<populacao[i]->getOWA()<<endl;
+	}
+
+
+}
