369de3ec867d91f31252f477bf76ab4d5577a58e
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-10-23 21:06:33 -0300

    Resultados

---
+++ memeticoSPEA_X_transgenetic/8_OBJETIVOS/lerTempo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for p in 30 35 40 45 50 100 200 300 400 500 600 700 800 900 1000
+do
+	for i in 1 2 3
+	do #transgenetic
+		./lerTempo ../../transgenetic/Testes/8_OBJETIVOS/$p/Hurwicz/anticorrelated/$p.anticorr"$i"/$p.anticorr"$i"_timeFile.log teste.log 
+	done
+
+done
--- memeticoSPEA_X_transgenetic/8_OBJETIVOS/scriptTesteStatistico.sh
+++ memeticoSPEA_X_transgenetic/8_OBJETIVOS/scriptTesteStatistico.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
-for p in 30 35 40 45 50 100 200 300 400 500 600 700 800 900 #1000
+for p in 1000
 do
 	for i in 1 2 3
 	do
-		cat ../../memetic_SEPA_R/Testes/8_OBJETIVOS/$p/Hurwicz/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log ../../transgenetic/Testes/8_OBJETIVOS/$p/Hurwicz/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log > inputFilter.in
-		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt Hurwicz/anticorrelated/mann_whit_$p.anticorr$i.out
+		cat ../../memetic_SEPA_R/Testes/8_OBJETIVOS/$p/k_centrum/correlated/$p.corr$i/$p.corr"$i"_sampleFile.log ../../transgenetic/Testes/8_OBJETIVOS/$p/k_centrum/correlated/$p.corr$i/$p.corr"$i"_sampleFile.log > inputFilter.in
+		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt k_centrum/correlated/mann_whit_$p.corr$i.out
+		
+		cat ../../memetic_SEPA_R/Testes/8_OBJETIVOS/$p/k_centrum/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log ../../transgenetic/Testes/8_OBJETIVOS/$p/k_centrum/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log > inputFilter.in
+		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt k_centrum/anticorrelated/mann_whit_$p.anticorr$i.out
 		
 	done
 done
