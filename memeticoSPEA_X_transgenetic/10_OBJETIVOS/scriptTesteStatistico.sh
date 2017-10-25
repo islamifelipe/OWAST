ab2cd5ce3ce4dbd3bd7ff93b90f47ee1c99b16e3
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-10-25 14:32:54 -0300

    resultados

--- memeticoSPEA_X_transgenetic/10_OBJETIVOS/scriptTesteStatistico.sh
+++ memeticoSPEA_X_transgenetic/10_OBJETIVOS/scriptTesteStatistico.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-for p in 35 40 45 50 100 200 300 400 500 600 700 800 #900 1000
+for p in 900 1000
 do
 	for i in 1 2 3
 	do
-		cat ../../memetic_SEPA_R/Testes/10_OBJETIVOS/$p/k_trimmed/correlated/$p.corr$i/$p.corr"$i"_sampleFile.log ../../transgenetic/Testes/10_OBJETIVOS/$p/k_trimmed/correlated/$p.corr$i/$p.corr"$i"_sampleFile.log > inputFilter.in
-		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt k_trimmed/correlated/mann_whit_$p.corr$i.out
+		cat ../../memetic_SEPA_R/Testes/10_OBJETIVOS/$p/k_centrum/correlated/$p.corr$i/$p.corr"$i"_sampleFile.log ../../transgenetic/Testes/10_OBJETIVOS/$p/k_centrum/correlated/$p.corr$i/$p.corr"$i"_sampleFile.log > inputFilter.in
+		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt k_centrum/correlated/mann_whit_$p.corr$i.out
 		
-		cat ../../memetic_SEPA_R/Testes/10_OBJETIVOS/$p/k_trimmed/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log ../../transgenetic/Testes/10_OBJETIVOS/$p/k_trimmed/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log > inputFilter.in
-		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt k_trimmed/anticorrelated/mann_whit_$p.anticorr$i.out
+		cat ../../memetic_SEPA_R/Testes/10_OBJETIVOS/$p/k_centrum/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log ../../transgenetic/Testes/10_OBJETIVOS/$p/k_centrum/anticorrelated/$p.anticorr$i/$p.anticorr"$i"_sampleFile.log > inputFilter.in
+		../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/mann-whit inputFilter.in ../../../../Pesquisa_AGM/Epsilon/PISA/statistics_c_source/param.txt k_centrum/anticorrelated/mann_whit_$p.anticorr$i.out
 		
 	done
 done
--- memeticoSPEA_X_transgenetic/8_OBJETIVOS/lerPvalores.sh
+++ memeticoSPEA_X_transgenetic/8_OBJETIVOS/lerPvalores.sh
@@ -3,7 +3,7 @@ for p in 30 35 40 45 50 100 200 300 400 500 600 700 800 900 1000
 do
 	for i in 1 2 3
 	do
-		./lerPvalores Hurwicz/anticorrelated/mann_whit_"$p".anticorr"$i".out teste.log
+		./lerPvalores k_trimmed/correlated/mann_whit_"$p".corr"$i".out teste.log
 	done
 
 done
