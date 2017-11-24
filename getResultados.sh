bd986961fe6146160705323bfdb811cf21898154
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-11-24 14:19:50 -0300

    resultados

--- getResultados.sh
+++ getResultados.sh
@@ -3,7 +3,7 @@
 
 for i in 30 35 40 45 50 100 200 300 400 
 do
-	./getResultados < INSTANCES/8_OBJETIVOS/TESTE/fernandes2/Hurwicz/correlated/$i.corr1.out
-	./getResultados < INSTANCES/8_OBJETIVOS/TESTE/fernandes2/Hurwicz/correlated/$i.corr2.out
-	./getResultados < INSTANCES/8_OBJETIVOS/TESTE/fernandes2/Hurwicz/correlated/$i.corr3.out
+	./getResultados < INSTANCES/10_OBJETIVOS/TESTE/fernandes2/Hurwicz/anticorrelated/$i.anticorr1.out
+	./getResultados < INSTANCES/10_OBJETIVOS/TESTE/fernandes2/Hurwicz/anticorrelated/$i.anticorr2.out
+	./getResultados < INSTANCES/10_OBJETIVOS/TESTE/fernandes2/Hurwicz/anticorrelated/$i.anticorr3.out
 done 
