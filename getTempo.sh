f43308c0164c7c50ac1a63e17769b4385e1eb760
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2018-01-19 14:12:58 -0300

    resultados

--- getTempo.sh
+++ getTempo.sh
@@ -6,14 +6,25 @@ do
 	c=1
 	while [ $c -le 30 ]; 
     do
-    		./getTempo <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr1/$i.corr1_"$c".out  >> transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr1/$i.corr1_tempoProOtimoo.log
-			./getTempo <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr2/$i.corr2_"$c".out  >> transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr2/$i.corr2_tempoProOtimoo.log
-			./getTempo <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr3/$i.corr3_"$c".out  >> transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr3/$i.corr3_tempoProOtimoo.log
+    		./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/correlated/$i.corr1/$i.corr1_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/correlated/$i.corr1/$i.corr1_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/correlated/$i.corr2/$i.corr2_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/correlated/$i.corr2/$i.corr2_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/correlated/$i.corr3/$i.corr3_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/correlated/$i.corr3/$i.corr3_tempoProOtimoo.log
 			
 
-    		./getTempo <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr1/$i.anticorr1_"$c".out  >> transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr1/$i.anticorr1_tempoProOtimoo.log
-			./getTempo <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr2/$i.anticorr2_"$c".out  >> transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr2/$i.anticorr2_tempoProOtimoo.log
-			./getTempo <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr3/$i.anticorr3_"$c".out  >> transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr3/$i.anticorr3_tempoProOtimoo.log
+    		./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/anticorrelated/$i.anticorr1/$i.anticorr1_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/anticorrelated/$i.anticorr1/$i.anticorr1_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/anticorrelated/$i.anticorr2/$i.anticorr2_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/anticorrelated/$i.anticorr2/$i.anticorr2_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/anticorrelated/$i.anticorr3/$i.anticorr3_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_trimmed/anticorrelated/$i.anticorr3/$i.anticorr3_tempoProOtimoo.log
+			
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr1/$i.corr1_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr1/$i.corr1_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr2/$i.corr2_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr2/$i.corr2_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr3/$i.corr3_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr3/$i.corr3_tempoProOtimoo.log
+			
+
+    		./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr1/$i.anticorr1_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr1/$i.anticorr1_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr2/$i.anticorr2_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr2/$i.anticorr2_tempoProOtimoo.log
+			./getTempo <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr3/$i.anticorr3_"$c".out  >> memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr3/$i.anticorr3_tempoProOtimoo.log
+			
+
 			c=$((c+1))
     done
 done 
