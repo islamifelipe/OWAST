607f86874617816f1da19e54245748170818eff5
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2018-01-20 09:21:09 -0300

    resultados

--- getResultado2.sh
+++ getResultado2.sh
@@ -3,16 +3,19 @@
 
 for i in 30 35 40 45 50 100 200 300 400 500 600 700 800 900 1000
 do 
-	./getResultado2 <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr1/$i.corr1_timeFile.log
-	./getResultado2 <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr2/$i.corr2_timeFile.log
-	./getResultado2 <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/correlated/$i.corr3/$i.corr3_timeFile.log
+	./getResultado2 <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr1/$i.corr1_sampleFile.log
+	./getResultado2 <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr2/$i.corr2_sampleFile.log
+	./getResultado2 <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/correlated/$i.corr3/$i.corr3_sampleFile.log
 done 
 
 echo "\n"
 
 for i in 30 35 40 45 50 100 200 300 400 500 600 700 800 900 1000
 do 
-	./getResultado2 <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr1/$i.anticorr1_timeFile.log
-	./getResultado2 <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr2/$i.anticorr2_timeFile.log
-	./getResultado2 <  transgenetic64bit/Testes/10_OBJETIVOS/$i/Hurwicz/anticorrelated/$i.anticorr3/$i.anticorr3_timeFile.log
+	./getResultado2 <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr1/$i.anticorr1_sampleFile.log
+	./getResultado2 <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr2/$i.anticorr2_sampleFile.log
+	./getResultado2 <  memetic_SEPA_R_64bit/Testes/10_OBJETIVOS/$i/k_centrum/anticorrelated/$i.anticorr3/$i.anticorr3_sampleFile.log
 done 
+
+# memetic_SEPA_R_64bit
+# transgenetic64bit
--- memeticoSPEA_X_transgenetic__64bit/10_OBJETIVOS - copie/lerTempo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for p in 30 35 40 45 50 100 200 300 400 500 600 700 800 900 1000
-do
-	for i in 1 2 3
-	do #transgenetic #memetic_SEPA_R
-		./lerTempo ../../transgenetic/Testes/10_OBJETIVOS/$p/Hurwicz/anticorrelated/$p.anticorr"$i"/$p.anticorr"$i"_timeFile.log teste.log 
-	done
-
-done
